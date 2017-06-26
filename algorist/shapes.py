4db411cd1cca3904bf7ea2f373e8d8634e887dae
pmende <patrick.c.mende@gmail.com>
2017-06-26 08:58:31 -0700

    create outline for shapes module

---
+++ algorist/shapes.py
@@ -0,0 +1,117 @@
+'''Module for creating shapes for use with a Surface instance
+
+CLASSES
+-------
+Shape: UNIMPLEMENTED
+
+FUNCTIONS
+---------
+line: UNIMPLEMENTED
+polyline: UNIMPLEMENTED
+arrow: UNIMPLEMENTED
+polyarrow: UNIMPLEMENTED
+square: UNIMPLEMENTED
+rect: UNIMPLEMENTED
+arc: UNIMPLEMENTED
+circle: UNIMPLEMENTED
+ellipse: UNIMPLEMENTED
+bezier: UNIMPLEMENTED
+bezier_arrow: UNIMPLEMENTED
+regular_ngon: UNIMPLEMENTED
+polygon: UNIMPLEMENTED
+'''
+
+class Shape(object):
+    '''Objects used to draw on surfaces
+
+    Attributes
+    ----------
+    '''
+
+    def __init__(self):
+        pass
+
+class ShapeGroup(object):
+    '''
+    '''
+
+    def __init__(self):
+        pass
+
+def line():
+    '''
+    '''
+
+    pass
+
+def polyline():
+    '''
+    '''
+
+    pass
+
+def arrow():
+    '''
+    '''
+
+    pass
+
+def polyarrow():
+    '''
+    '''
+
+    pass
+
+def square():
+    '''
+    '''
+
+    pass
+
+def rect():
+    '''
+    '''
+
+    pass
+
+def arc():
+    '''
+    '''
+
+    pass
+
+def circle():
+    '''
+    '''
+
+    pass
+
+def ellipse():
+    '''
+    '''
+
+    pass
+
+def bezier():
+    '''
+    '''
+
+    pass
+
+def bezier_arrow():
+    '''
+    '''
+
+    pass
+
+def regular_ngon():
+    '''
+    '''
+
+    pass
+
+def polygon():
+    '''
+    '''
+
+    pass
