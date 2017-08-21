84389eadb0d44a65fb3dbea1a86f5aecfee3742d
pmende <patrick.c.mende@gmail.com>
2017-08-21 14:50:18 -0700

    add surface module docstring

---
+++ algorist/surfaces.py
@@ -0,0 +1,9 @@
+'''Module of Surface objects that are the base cavas
+
+CLASSES
+-------
+ImageSurface: UNIMPLEMENTED
+PDFSurface: UNIMPLEMENTED
+PSSurface: UNIMPLEMENTED
+SVGSurface: UNIMPLEMENTED
+'''
