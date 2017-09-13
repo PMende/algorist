a9b8379acedf785160080b1e7eb93affd24934f4
pmende <patrick.c.mende@gmail.com>
2017-09-13 16:44:47 -0700

    create minimal skeleton for Surface parent class

--- algorist/surfaces.py
+++ algorist/surfaces.py
@@ -12,19 +12,31 @@ SVGSurface: UNIMPLEMENTED
 class Surface():
     '''Base canvas-like object to be drawn on
     '''
-    def __init__(self):
+    def __init__(self, *, name=None, shapes=None):
+        if shapes is None:
+            self.shapes = []
+        else:
+            self.shapes = shapes
+
+    def render(self):
         pass
+
+    def write(self):
+        pass
+
+    def add(shape):
+        self.
 
 class ImageSurface(Surface):
     '''
     '''
-    def __init__(self):
+    def __init__(self, size, *, name=None, **kwargs):
         pass
 
 class PSSurface(Surface):
     '''
     '''
-    def __init__(self):
+    def __init__(self, size, *, name):
         pass
 
 class SVGSurface(Surface):
